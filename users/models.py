6b3c0a9e42f08f35df03f3e5cb1b861a6fcc35f3
namy <namy_park@protonmail.com>
2021-08-03 14:27:23 +0100

    added signals to the user app

--- users/models.py
+++ users/models.py
@@ -3,15 +3,16 @@ from django.contrib.auth.models import User
 import uuid
 
 # Create your models here.
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    username = models.CharField(max_length=20,blank=True,null=True)
-    location  = models.CharField(max_length=20,blank=True,null=True)
+    username = models.CharField(max_length=20, blank=True, null=True)
+    location = models.CharField(max_length=20, blank=True, null=True)
     name = models.CharField(blank=True, null=True, max_length=100)
     email = models.EmailField(max_length=100, null=True, blank=True)
     short_intro = models.CharField(blank=True, null=False, max_length=200)
     bio = models.TextField(blank=True, null=True)
-    profile_image = models.ImageField( 
+    profile_image = models.ImageField(
         blank=True,
         null=True,
         upload_to="profiles/",
@@ -27,16 +28,23 @@ class Profile(models.Model):
     )
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
-class  Skill(models.Model):
-    owner = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True,blank=True)
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=20)
-    description = models.TextField(blank=True,null=True,max_length=500)
+    description = models.TextField(blank=True, null=True, max_length=500)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(
         default=uuid.uuid4, unique=True, primary_key=True, editable=False
     )
 
     def __str__(self):
-        return self.name
+        return str(self.name)
+
+
+
+    
+
+
